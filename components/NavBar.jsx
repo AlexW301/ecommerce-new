e0fb22d5535789e59160acdc386fa69922bd26b7
Alex Waller <alex@pmlhomeloans.com>
2022-05-18 13:40:33 -0400

    Initial commit

---
+++ components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+import { AiOutlineShopping, AIOutlineShopping } from 'react-icons/ai'
+
+import { Cart } from './'
+import { useStateContext } from '../context/StateContext'
+
+function NavBar() {
+  const { showCart, setShowCart, totalQuantities } = useStateContext()
+  return (
+    <div className='navbar-container'>
+      <p>
+        <Link href={'/'}>
+          JSM Headphones
+        </Link>
+      </p>
+
+      <button type="button" className='cart-icon' onClick={() => setShowCart(true)}>
+          <AiOutlineShopping />
+          <span className='cart-item-qty'>{totalQuantities}</span>
+      </button>
+      {showCart && <Cart />}
+    </div>
+  )
+}
+
+export default NavBar
